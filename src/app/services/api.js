edadab8a3f8acc4a91730a00f42201cc60a27e8d
Mayara Soares <141755987+maydreadful@users.noreply.github.com>
2026-03-04 23:06:07 -0300

    correcoes axios

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -1,7 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // URL do backend
+  // ESSA LINHA É A CHAVE: Ela diz para o site usar a variável da Vercel
+  // Se ela não existir, ele usa o localhost (por isso está falhando)
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
 });
 
 export default api;
